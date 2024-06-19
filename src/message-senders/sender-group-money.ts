0d8bae408e6e68c3b425de45dfd231403bc4a976
Francisco Felipe <46639197+felmp@users.noreply.github.com>
2024-06-18 23:52:24 -0300

    feat: remove gpt

--- src/message-senders/sender-group-money.ts
+++ src/message-senders/sender-group-money.ts
@@ -4,40 +4,28 @@ import { gpt, wpp } from '../axios';
 
 export function sendMoneyMessage(message: string, res: FastifyReply) {
 
-  const prompt = "Preciso que reformule um texto para uma postagem de um GRUPO DE ALERTAS DE PASSAGENS, chamado @FLYALERTAS. "
-    + "Envie somente 1 mensagem formatada e preparada para o envio ao whatsapp "
-    + "Ao invês de 'Experimente luxo, flexibilidade e arranjos de viagem sem complicações. Reserve sua próxima aventura conosco e faça cada milha valer a pena!' fique avontade para escrever qualquer descrição apropriada para viagem, inclua curiosidades sobre o local de destino ou coisas para se fazer nele, seja criativo."
-    + "Altere completamente a estrutura e os emojis, sem prejudicar as informações. Envie sempre o valor, o local, a classe, o programa de afiliados e todas as informações"
-    + "Faça com que fique entendivel e completamente legivel para meu cliente. "
-    + "Apague qualquer link ou redirecionamento para site, ou qualquer terminologia ou termo: HTTPS HTTP http www .com .br cartões aluno live "
+  // const prompt = "Preciso que reformule um texto para uma postagem de um GRUPO DE ALERTAS DE PASSAGENS, chamado @FLYALERTAS. "
+  //   + "Envie somente 1 mensagem formatada e preparada para o envio ao whatsapp "
+  //   + "Ao invês de 'Experimente luxo, flexibilidade e arranjos de viagem sem complicações. Reserve sua próxima aventura conosco e faça cada milha valer a pena!' fique avontade para escrever qualquer descrição apropriada para viagem, inclua curiosidades sobre o local de destino ou coisas para se fazer nele, seja criativo."
+  //   + "Altere completamente a estrutura e os emojis, sem prejudicar as informações. Envie sempre o valor, o local, a classe, o programa de afiliados e todas as informações"
+  //   + "Faça com que fique entendivel e completamente legivel para meu cliente. "
+  //   + "Apague qualquer link ou redirecionamento para site, ou qualquer terminologia ou termo: HTTPS HTTP http www .com .br cartões aluno live "
 
 
   if (message !== 'Programa de afiliados não encontrado') {
-    const data = {
-      'model': 'gpt-3.5-turbo',
-      'messages': [
-        { 'role': 'user', 'content': prompt },
-        { 'role': 'user', 'content': message }
-      ]
-    }
 
-    gpt.post('chat/completions', data).then((response) => {
-      var data = JSON.stringify({
-        "to_group_uuid": "WAG2a2d7898-305f-4b21-8528-b26f36f3a342", //grupo para testes em real
-        "from_number": "+5579920012363",
-        "text": response.data.choices[0].message.content
+    var data = JSON.stringify({
+      "to_group_uuid": "WAG2a2d7898-305f-4b21-8528-b26f36f3a342", //grupo para testes em real
+      "from_number": "+5579920012363",
+      "text": message
+    });
+
+    wpp.post('open/whatsapp/send-message', data)
+      .then(function (response) {
+        res.send(JSON.stringify(response.data))
+      })
+      .catch(function (error) {
+        console.log(error);
       });
-
-      wpp.post('open/whatsapp/send-message', data)
-        .then(function (response) {
-          res.send(JSON.stringify(response.data))
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }).catch((err) => {
-      console.log(err)
-    })
-
   }
 }
